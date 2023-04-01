0fa51b6090093907396a63285cd427296e0df0b7
numhebzaba <num.ptp@gmail.com>
2023-04-01 22:44:29 +0700

    Add : Button between scences Add : generate light

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -8,4 +8,16 @@ public class SwitchScene : MonoBehaviour
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
+    public void CustomeGame()
+    {
+        SceneManager.LoadScene("Custom_Mode");
+    }
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
